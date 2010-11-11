2da0905d371a1ef949bf10338239490cdecb387d
Barnaba Turek <barnabaturek@gmail.com>
2010-11-11 21:44:25 +0100

    added drawableNode and drawableEdge

--- omelette/fromage/test/test_drawables.py
+++ omelette/fromage/test/test_drawables.py
@@ -1,32 +1,49 @@
 import unittest
+import random
 from mock import Mock
-from omelette.fromage.common import Drawable
+from omelette.fromage.common import *
 from omelette.parser.uml import UMLObject
 
 class UnitTest(unittest.TestCase):
 
 
     def setUp(self):
-        self.mock = Mock()    
+        self.mock = Mock()
+        self.class_ = random.choice([Drawable, DrawableNode, DrawableEdge])
 
     def test_properties(self):
+        "Caution: If this test behaves randomly it's because tested class is choosen randomly!"
         (key, val) = ("stereotype", "Interface")
-        instance = Drawable({key: val})
+        instance = self.class_({key: val})
         self.assertEqual(val, instance[key])
         
     def test_operations(self):
+        "Caution: If this test behaves randomly it's because tested class is choosen randomly!"
         operations = ["+a()", "+b()", "-c() : int"]
         self.mock.operations.return_value = operations
         
-        instance = Drawable(self.mock)
+        instance = self.class_(self.mock)
         self.assertEqual(operations, instance.operations())
         
     def test_attributes(self):
+        "Caution: If this test behaves randomly it's because tested class is choosen randomly!"
         attributes = ["+a", "+b", "-c : int"]
         self.mock.attributes.return_value = attributes
         
-        instance = Drawable(self.mock)
+        instance = self.class_(self.mock)
         self.assertEqual(attributes, instance.attributes())
+    
+    def test_has_anchors(self):
+        instance = DrawableEdge(None)
+        self.assertTrue(hasattr(instance,"source_anchor"))
+        self.assertTrue(hasattr(instance,"target_anchor"))
+        
+    def test_has_position(self):
+        instance = DrawableNode(None)
+        pos = (3,5)
+        instance.set_position(pos)
+        
+        self.assertEqual(pos, instance.get_position())
         
 class IntegrationTest(unittest.TestCase):
     
