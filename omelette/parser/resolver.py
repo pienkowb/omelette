c783dff5cdd21f456f7dc33bf27445e54e7afac2
Bartosz Pieńkowski <patyk9@gmail.com>
2010-11-27 16:07:49 +0100

    Created prototype of DependencyResolver class.

---
+++ omelette/parser/resolver.py
@@ -0,0 +1,6 @@
+class DependencyResolver:
+    def __init__(self, uml_objects):
+        self.__uml_objects = uml_objects
+
+    def resolve(self):
+        pass
---
+++ omelette/parser/test/test_resolver.py
@@ -0,0 +1,9 @@
+import unittest
+from omelette.parser.resolver import DependencyResolver
+
+class DependencyResolverTest(unittest.TestCase):
+    def test_resolve(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
