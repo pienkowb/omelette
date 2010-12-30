e9f4bf5f398a180bc71523008af2731d9ea852fd
Bartosz Pieńkowski <patyk9@gmail.com>
2010-12-31 00:16:45 +0100

    Refactored Code module.

--- omelette/parser/code.py
+++ omelette/parser/code.py
@@ -1,8 +1,27 @@
 class _CodeObject(object):
+    """Class containing the code of a single object."""
+
     def __init__(self, position, header):
         self.position = position
         self.lines = [header]
         self.modified = True
+
+    def insert_line(self, number, line):
+        self.lines.insert(number - self.position, line)
+        self.modified = True
+
+    def remove_line(self, number):
+        del self.lines[number - self.position]
+        self.modified = True
+
+    def transfer_lines(self, other, number):
+        position = number - self.position
+
+        if position in range(len(self.lines)):
+            other.lines.extend(self.lines[position:])
+            del self.lines[position:]
+
+            self.modified = other.modified = True
 
     def __cmp__(self, other):
         return cmp(self.position, other.position)
@@ -20,39 +39,41 @@ def _is_header(line):
 
 
 class Code(object):
+    """Class representing the code divided into objects."""
+
     def __init__(self):
         self.__objects = [_CodeObject(-1, "")]
 
     def objects(self, condition=None):
         return filter(condition, self.__objects)
 
-    def insert_line(self, number, line):
-        for object in self.objects(_after(number)):
-            object.position += 1
+    def __shift(self, position, offset):
+        for object in self.objects(_after(position)):
+            object.position += offset
 
-        previous = self.objects(_before(number))[-1]
-        position = number - previous.position
+    def insert_line(self, number, line):
+        self.__shift(number, 1)
 
         if _is_header(line):
             object = _CodeObject(number, line)
 
-            object.lines.extend(previous.lines[position:])
-            del previous.lines[position:]
+            previous = self.objects(_before(number))[-1]
+            previous.transfer_lines(object, number)
 
             self.__objects.append(object)
             self.__objects.sort()
         else:
-            previous.lines.insert(position, line)
+            object = self.objects(_before(number))[-1]
+            object.insert_line(number, line)
 
     def remove_line(self, number):
         object = self.objects(_before(number))[-1]
-        del object.lines[number - object.position]
+        object.remove_line(number)
 
         if object.position == number:
             previous = self.objects(_before(number))[-2]
-            previous.lines.extend(object.lines)
+            object.transfer_lines(previous, number)
 
             self.__objects.remove(object)
 
-        for object in self.objects(_after(number)):
-            object.position -= 1
+        self.__shift(number, -1)
