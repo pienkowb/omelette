92bd2b1278d974a214f6f6db555b4020b00352cc
Barnaba Turek <barnabaturek@gmail.com>
2011-02-27 19:38:14 +0100

    umlobject recovered from github

--- omelette/compiler/uml.py
+++ omelette/compiler/uml.py
@@ -1,75 +1,90 @@
 class UMLObject(object):
     """Class representing UML diagram object."""
   
-    def __init__(self):
+    def __init__(self, parent=None, name=None, is_prototype=False):
         self.__operations = []
         self.__attributes = []
-        self.__properties = {}
+        self.properties = {}
         
-        self.root = None
-        self.predecessor = None
-        self.name = None
+        self.type = None
+        self.parent = parent
+        self.name = name
+        self.is_prototype = is_prototype
         
     def __setitem__(self, key, value):
-        self.__properties[key] = value
+        self.properties[key] = value
         
     def __getitem__(self, key):
-        return self.__properties[key]
+        return self.properties[key]
 
     def add_operation(self, operation):
-        self.__operations.append(UMLOperation(operation))
+        self.__operations.append(operation)
 
     def add_attribute(self, attribute):
-        self.__attributes.append(UMLAttribute(attribute))
+        self.__attributes.append(attribute)
         
     def operations(self):
-        operations = self.__operations
-        operations.sort()
-        return map(str, operations)
+        return self.__operations
         
     def attributes(self):
-        attributes = self.__attributes
-        attributes.sort()
-        return map(str, attributes)
-                    
+        return self.__attributes
+    
+    def __eq__(self, other) : 
+        return self.__dict__ == other.__dict__
+
+          
+def _format_if_not_none(format, value):
+    if value is None:
+        return ""
+    else:
+        return format % value          
+
                     
 class _Field(object):    
     """
-    Helper class used in UMLObject. Provides __cmp__ for methods and attributes.
+    Helper class used in UMLObject..
     """
     
-    scope_order = ["+", "~", "#", "-"]
-    
-    def __init__(self, content):
-        self.content = content
-        self.scope = self.__get_scope()
-        self.identifier = self.__get_identifier()
-        
-    def __get_scope(self):
-        return self.content[0]
-    
-    def __get_identifier(self):
-        return self.content[1:]
-    
-    def __str__(self):
-        return self.content
-        
-    def __cmp__(self, other):
-        result = self.__cmp_by_scope(other)
-        return result or cmp(self.identifier, other.identifier)
-    
-    def __cmp_by_scope(self, other):
-        order = _Field.scope_order
-        return  order.index(self.scope) - \
-                order.index(other.scope) 
+    def __eq__(self, other) : 
+        return self.__dict__ == other.__dict__
 
 
-class UMLOperation(_Field):
+class Operation(_Field):
     """Class representing UML Operation."""
-    pass
+    
+    def __init__(self, is_static, scope, name, parameters, type):
+        self.is_static = is_static
+        self.scope = scope
+        self.name = name
+        self.parameters = parameters
+        self.type = type
+
+    def __str__(self):
+        return self.scope + self.name + "(" + \
+                self.__formatted_param_list() + ")" + \
+                _format_if_not_none(" : %s", self.type)   
+        
+    def __formatted_param_list(self):
+        format = lambda(n, t) : n + _format_if_not_none(" : %s", t)
+        formatted = map(format, self.parameters)
+        return ", ".join(formatted)
+                
+    def __format_parameter(self, parameter):
+        (name, type) = parameter
+        return name + (type and (" : " + type) or "")
 
 
-class UMLAttribute(_Field):
+class Attribute(_Field):
     """Class representing UML Attribute."""
-    pass
-
+    
+    def __init__(self, is_static, scope, name, type, default_value):
+        self.is_static = is_static
+        self.scope = scope
+        self.name = name
+        self.type = type
+        self.default_value = default_value
+        
+    def __str__(self):
+        return self.scope + self.name + \
+                _format_if_not_none(" : %s", self.type) + \
+                _format_if_not_none(" = %s", self.default_value)
