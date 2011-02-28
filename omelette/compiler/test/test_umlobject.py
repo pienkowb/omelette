65863e4bddecb5381d9fb447fce5a5aad26af587
Barnaba Turek <barnabaturek@gmail.com>
2011-02-28 21:49:52 +0100

    recovered umlobject's tests from demo

--- omelette/compiler/test/test_umlobject.py
+++ omelette/compiler/test/test_umlobject.py
@@ -1,55 +1,85 @@
 import unittest
 from omelette.compiler.uml import UMLObject
-from mock import Mock
+from mock import Mock, MagicMock
 
-class Test(unittest.TestCase):
+class TestUMLObject(unittest.TestCase):
+
+    def test_constructor(self):
+        o = UMLObject("fasada", "asd", True)
+        self.assertEquals(o.parent, "fasada")
+        self.assertEquals(o.name, "asd")
+        self.assertTrue(o.is_prototype)
+        
+    def test_eq(self):
+        instance = UMLObject("fasada", "asd", True)
+        other = UMLObject("fasada", "asd", True)
+        instance["a"] = 3
+        other["a"] = 3
+        
+        self.assertEquals(instance, other)
+        
+    def test_not_eq_field(self):
+        instance = UMLObject("fasada", "asd", False)
+        other = UMLObject("fasada", "asd", True)
+        
+        self.assertNotEqual(instance, other)
+        
+    def test_not_eq_property(self):
+        instance = UMLObject()
+        other = UMLObject()
+        instance["a"] = 3
+        other["a"] = 4
+        
+        self.assertNotEqual(instance, other)
+        
+    def test_not_eq_operation(self):
+        instance = UMLObject()
+        other = UMLObject()
+        (o1, o2) = (Mock(), Mock())
+        instance.add_operation(o1)
+        instance.add_operation(o2)
+        
+        self.assertNotEqual(instance, other)
+        
+    def test_eq_operation(self):
+        instance = UMLObject()
+        other = UMLObject()
+        o1 = MagicMock()
+        o1.__eq__.return_value = True
+        o2 = MagicMock()
+        o2.__eq__.return_value = True
+        instance.add_operation(o1)
+        other.add_operation(o2)
+        
+        self.assertEqual(instance, other)
+        
 
     def test_operation(self):
         instance = UMLObject()
-        operation = "+operation()"
-        
+        operation = Mock()
         instance.add_operation(operation)
         result = instance.operations()[0]
         
-        self.assertEquals(result, operation)
-        
+        self.assertEquals(result, operation)  
+    
     def test_attribute(self):
         instance = UMLObject()
-        attribute = "+attribute"
+        attribute = Mock()
         
         instance.add_attribute(attribute)
         result = instance.attributes()[0]
         
-        self.assertEquals(attribute, result)
-        
-    def test_sorting(self):
-        instance = UMLObject()
-        data = ["+a", "-z", "#z", "#c", "-b", "+c"]
-        
-        [instance.add_attribute(attr) for attr in data]
-        [instance.add_operation(op+"()") for op in data]
-        
-        attrs = instance.attributes()
-        ops = instance.operations()
-        
-        self.assertEquals(["+a", "+c", "#c","#z","-b","-z"], attrs)
-        self.assertEquals(["+a()", "+c()", "#c()","#z()","-b()","-z()"], ops)
+        self.assertEquals(result, attribute)
         
     def test_property(self):
-        property   = "stereotype"
-        value      = "Interface"
+        property = "stereotype"
+        value = "Interface"
         instance = UMLObject()
         
         instance[property] = value
         
         self.assertEquals(value, instance[property])
-        
-    def test_root_property(self):
-        instance = UMLObject()
-        m = Mock()
-        
-        instance.root = m
-        self.assertEqual(m, instance.root)
+
         
 
 if __name__ == "__main__":
