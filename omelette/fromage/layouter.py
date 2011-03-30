a86f7377e24ac67ffbac022698040457bb2a881a
Piotr Piechal <piechalp@gmail.com>
2011-03-30 12:20:20 +0200

    Circular layouter without tests

--- omelette/fromage/layouter.py
+++ omelette/fromage/layouter.py
@@ -1,14 +1,20 @@
 class Layouter(object):
     @staticmethod
     def layout(diagram):
-        x = y = highest = 0;
-        for node in diagram.nodes.values():
-            node.moveBy(x, y)
-            x += 20 + node.boundingRect().size().width()
-            if node.boundingRect().size().height() > highest:
-                    highest = node.boundingRect().size().height()
-            if x > 400:
-                x = 0
-                y += highest + 20
-                highest = 0
-
+	print "hello"
+        sx = sy = 0
+        left = bottom = 0
+	r = 0
+	greatestwidth = max( diagram.nodes.values().boundingRect().size().width() )
+	greatestheight = max( diagram.nodes.values().boundingRect().size().height() )
+	r = 2 * max( greatestwidth, greatestheight )
+	print r
+	n = diagram.nodes.len()
+	rad = 2 * math.pi / n
+	print rad
+	i = 0
+	for node in diagram.nodes.values():
+            x = sx + r * math.sin( rad * i )
+	    y = sy + r * math.cos( rad * i++ )
+	    print "(%f, %f)" % (x, y)
+	    node.moveBy(x, y)
