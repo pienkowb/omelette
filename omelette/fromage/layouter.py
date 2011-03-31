27edd57a86cc8dd2ccecf3a5afb6a68264be89a0
Piotr Piechal <piechalp@gmail.com>
2011-03-31 20:04:11 +0200

    Fixed problem with large n, added parameters for start angle and spread factor, comments improved, lines wrapped

--- omelette/fromage/layouter.py
+++ omelette/fromage/layouter.py
@@ -2,29 +2,29 @@ import math
 class Layouter(object):
     @staticmethod
     def layout(diagram):
-# TODO add parameter for start angle
-# TODO tidy up comments
-# TODO refactor code
-# TODO add parameter for spread factor
         sx = sy = 200 # Defining center of circle
 	r = 0
-	greatestwidth = greatestheight = 0
-	for node in diagram.nodes.values(): # Finding greatest size of drawable
-		if greatestwidth < node.boundingRect().size().width():
-			greatestwidth = node.boundingRect().size().width()
-		if greatestheight < node.boundingRect().size().height():
-			greatestheight = node.boundingRect().size().height()
+	maxwidth = maxheight = 0
+	start = math.pi / 2 # Starting angle
+	spread = 2 # Diagram nodes spread factor
+	for node in diagram.nodes.values(): # Finding max size of drawable
+		if maxwidth < node.boundingRect().size().width():
+			maxwidth = node.boundingRect().size().width()
+		if maxheight < node.boundingRect().size().height():
+			maxheight = node.boundingRect().size().height()
 	n = len(diagram.nodes) # number of drawables
 	if n > 1:
-		rad = 2 * math.pi / n # calculating angle
-		if math.sin(rad) < 0.1:
-			r = 2 * max(greatestwidth, greatestheight)
+		angle = 2 * math.pi / n # calculating angle step
+		if angle % math.pi == 0:
+			r = spread * max(maxwidth, maxheight)
 		else:
-			r = 2 * max(greatestwidth, greatestheight) / math.sin( rad )
+			r = spread * max(maxwidth, maxheight) / math.sin( angle )
 		i = 0 # number of actual drawable
 		for node in diagram.nodes.values():
-			x = sx + r * math.sin(rad * i) # calculating xpos
-			y = sy + r * math.cos(rad * i) # calculating ypos
+			# calculating xpos
+			x = sx + r * math.sin(start + angle * i)
+			# calculating ypos
+			y = sy + r * math.cos(start + angle * i)
 			i += 1
 			node.moveBy(x, y)
 	else:
