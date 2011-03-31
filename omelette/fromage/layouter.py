0acf7c827bb518735960c193b5f8577c1f00a331
Piotr Piechal <piechalp@gmail.com>
2011-03-31 12:40:16 +0200

    Working circular layouter without tests

--- omelette/fromage/layouter.py
+++ omelette/fromage/layouter.py
@@ -1,20 +1,21 @@
+import math
 class Layouter(object):
     @staticmethod
     def layout(diagram):
-	print "hello"
-        sx = sy = 0
-        left = bottom = 0
+        sx = sy = 200 %Defining center of circle
 	r = 0
-	greatestwidth = max( diagram.nodes.values().boundingRect().size().width() )
-	greatestheight = max( diagram.nodes.values().boundingRect().size().height() )
+	greatestwidth = greatestheight = 0
+	for node in diagram.nodes.values(): %Finding greatest size of drawable
+		if greatestwidth < node.boundingRect().size().width():
+			greatestwidth = node.boundingRect().size().width()
+		if greatestheight < node.boundingRect().size().height():
+			greatestheight = node.boundingRect().size().height()
 	r = 2 * max( greatestwidth, greatestheight )
-	print r
-	n = diagram.nodes.len()
-	rad = 2 * math.pi / n
-	print rad
-	i = 0
+	n = len(diagram.nodes) %number of drawables
+	rad = 2 * math.pi / n %calculating radian
+	i = 0 %number of actual drawable
 	for node in diagram.nodes.values():
-            x = sx + r * math.sin( rad * i )
-	    y = sy + r * math.cos( rad * i++ )
-	    print "(%f, %f)" % (x, y)
+            x = sx + r * math.sin( rad * i ) %calculating xpos
+	    y = sy + r * math.cos( rad * i ) %calculating ypos
+	    i += 1
 	    node.moveBy(x, y)
