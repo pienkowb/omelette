38cda13c9ed09abcd12acdff9b39a1ce4201cbaf
Piotr Piechal <piechalp@gmail.com>
2011-03-31 19:38:37 +0200

    Removed spaces near braces and added space after # of comment

--- omelette/fromage/layouter.py
+++ omelette/fromage/layouter.py
@@ -2,28 +2,29 @@ import math
 class Layouter(object):
     @staticmethod
     def layout(diagram):
-#TODO add parameter for start angle
-#TODO tidy up comments
-#TODO refactor code
-        sx = sy = 200 #Defining center of circle
+# TODO add parameter for start angle
+# TODO tidy up comments
+# TODO refactor code
+# TODO add parameter for spread factor
+        sx = sy = 200 # Defining center of circle
 	r = 0
 	greatestwidth = greatestheight = 0
-	for node in diagram.nodes.values(): #Finding greatest size of drawable
+	for node in diagram.nodes.values(): # Finding greatest size of drawable
 		if greatestwidth < node.boundingRect().size().width():
 			greatestwidth = node.boundingRect().size().width()
 		if greatestheight < node.boundingRect().size().height():
 			greatestheight = node.boundingRect().size().height()
-	n = len(diagram.nodes) #number of drawables
+	n = len(diagram.nodes) # number of drawables
 	if n > 1:
-		rad = 2 * math.pi / n #calculating angle
-		if math.sin( rad ) < 0.1:
-			r = 2 * max( greatestwidth, greatestheight )
+		rad = 2 * math.pi / n # calculating angle
+		if math.sin(rad) < 0.1:
+			r = 2 * max(greatestwidth, greatestheight)
 		else:
-			r = 2 * max( greatestwidth, greatestheight ) / math.sin( rad )
-		i = 0 #number of actual drawable
+			r = 2 * max(greatestwidth, greatestheight) / math.sin( rad )
+		i = 0 # number of actual drawable
 		for node in diagram.nodes.values():
-			x = sx + r * math.sin( rad * i ) #calculating xpos
-			y = sy + r * math.cos( rad * i ) #calculating ypos
+			x = sx + r * math.sin(rad * i) # calculating xpos
+			y = sy + r * math.cos(rad * i) # calculating ypos
 			i += 1
 			node.moveBy(x, y)
 	else:
