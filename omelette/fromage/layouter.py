870301af3062a0c104aa47663266fa53db76ef0d
Piotr Piechal <piechalp@gmail.com>
2011-03-31 12:41:43 +0200

    Working circular layouter with comments

--- omelette/fromage/layouter.py
+++ omelette/fromage/layouter.py
@@ -2,20 +2,20 @@ import math
 class Layouter(object):
     @staticmethod
     def layout(diagram):
-        sx = sy = 200 %Defining center of circle
+        sx = sy = 200 #Defining center of circle
 	r = 0
 	greatestwidth = greatestheight = 0
-	for node in diagram.nodes.values(): %Finding greatest size of drawable
+	for node in diagram.nodes.values(): #Finding greatest size of drawable
 		if greatestwidth < node.boundingRect().size().width():
 			greatestwidth = node.boundingRect().size().width()
 		if greatestheight < node.boundingRect().size().height():
 			greatestheight = node.boundingRect().size().height()
 	r = 2 * max( greatestwidth, greatestheight )
-	n = len(diagram.nodes) %number of drawables
-	rad = 2 * math.pi / n %calculating radian
-	i = 0 %number of actual drawable
+	n = len(diagram.nodes) #number of drawables
+	rad = 2 * math.pi / n #calculating radian
+	i = 0 #number of actual drawable
 	for node in diagram.nodes.values():
-            x = sx + r * math.sin( rad * i ) %calculating xpos
-	    y = sy + r * math.cos( rad * i ) %calculating ypos
+            x = sx + r * math.sin( rad * i ) #calculating xpos
+	    y = sy + r * math.cos( rad * i ) #calculating ypos
 	    i += 1
 	    node.moveBy(x, y)
