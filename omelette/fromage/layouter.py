b446db3677c5e93c5e50bf62757cc8b59e8b14e1
Piotr Piechal <piechalp@gmail.com>
2011-03-31 13:38:54 +0200

    Circular layouter with improved range calculation

--- omelette/fromage/layouter.py
+++ omelette/fromage/layouter.py
@@ -2,6 +2,9 @@ import math
 class Layouter(object):
     @staticmethod
     def layout(diagram):
+#TODO add parameter for start angle
+#TODO tidy up comments
+#TODO refactor code
         sx = sy = 200 #Defining center of circle
 	r = 0
 	greatestwidth = greatestheight = 0
@@ -10,12 +13,18 @@ class Layouter(object):
 			greatestwidth = node.boundingRect().size().width()
 		if greatestheight < node.boundingRect().size().height():
 			greatestheight = node.boundingRect().size().height()
-	r = 2 * max( greatestwidth, greatestheight )
 	n = len(diagram.nodes) #number of drawables
-	rad = 2 * math.pi / n #calculating radian
-	i = 0 #number of actual drawable
-	for node in diagram.nodes.values():
-            x = sx + r * math.sin( rad * i ) #calculating xpos
-	    y = sy + r * math.cos( rad * i ) #calculating ypos
-	    i += 1
-	    node.moveBy(x, y)
+	if n > 1:
+		rad = 2 * math.pi / n #calculating angle
+		if math.sin( rad ) < 0.1:
+			r = 2 * max( greatestwidth, greatestheight )
+		else:
+			r = 2 * max( greatestwidth, greatestheight ) / math.sin( rad )
+		i = 0 #number of actual drawable
+		for node in diagram.nodes.values():
+			x = sx + r * math.sin( rad * i ) #calculating xpos
+			y = sy + r * math.cos( rad * i ) #calculating ypos
+			i += 1
+			node.moveBy(x, y)
+	else:
+		node.moveBy(sx, sy)
