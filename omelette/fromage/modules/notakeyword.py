c47652093f254f145f24bbc754234a52e5935caf
Michal Zochniak <michal@zapu.net>
2011-04-01 18:07:52 +0200

    Experimenting with relation text positioning

--- omelette/fromage/modules/notakeyword.py
+++ omelette/fromage/modules/notakeyword.py
@@ -19,7 +19,7 @@ class DrawableRelation(DrawableEdge, QGraphicsLineItem):
         self.__relationName = "aasdasd"
         
         self.__texts = {}
-        for tag in ['name']:
+        for tag in ['name', 'name2']:
             self.__texts[tag] = DrawableText(self)
             self.__texts[tag].setParentItem(self) 
         
@@ -60,7 +60,8 @@ class DrawableRelation(DrawableEdge, QGraphicsLineItem):
         self.__xmarg = math.sin(self.__angle)
         self.__ymarg = math.cos(self.__angle)
         
-        self.__update_text('name', 'Barnex', 0.5, 1)
+        self.__update_text('name', 'Bar', 0.5, 1)
+        self.__update_text('name2', 'nex', 0.1, -1)
         
     def __update_text(self, tag, text, pos, orientation):
         xPos = self.real_line().p1().x() + self.__distanceX * pos
@@ -68,7 +69,7 @@ class DrawableRelation(DrawableEdge, QGraphicsLineItem):
         
         if(self.__angle >= 0):
             if(orientation == -1):
-                xPos -= self.__fontMetrics.width(text) + self.__xmarg * 10
+                xPos -= self.__xmarg * 10
                 yPos += self.__ymarg * 10
             else:
                 xPos += self.__xmarg * 10
@@ -78,9 +79,10 @@ class DrawableRelation(DrawableEdge, QGraphicsLineItem):
                 xPos += -self.__xmarg * 10
                 yPos += self.__ymarg * 10
             else:
-                xPos -= self.__fontMetrics.width(text) - self.__xmarg * 10
+                xPos -= self.__xmarg * 10
                 yPos -= self.__ymarg * 10 + self.__fontMetrics.height()
                 
+        xPos -= self.__fontMetrics.width(text) / 2
         #rect = QRectF(xPos, yPos, self.__fontMetrics.width(text), self.__fontMetrics.height())        
         
         self.__texts[tag].setPos(xPos, yPos)
