de5d8d4c2f1434ec40943ca1de5fe56a4f55da31
Barnaba Turek <barnabaturek@gmail.com>
2011-04-03 18:41:58 +0200

    first approach to resolving relations

--- omelette/fromage/diagram.py
+++ omelette/fromage/diagram.py
@@ -1,6 +1,6 @@
 import inspect
 from PyQt4 import QtGui
-from omelette.fromage.common import DrawableNode, DrawableEdge
+from omelette.fromage.common import DrawableNode, DrawableEdge, Anchor
 
 def _import(name):
     module = __import__(name)
@@ -46,6 +46,34 @@ class Diagram(QtGui.QGraphicsScene):
         else:
             raise AttributeError("Tried to create an object from a corrupted " +
                 "module")
+
+    def set_anchors(self):
+        for edge in self.edges.values():
+            source, target = self.__get_nodes(edge)
+            source_anchor, target_anchor = self.__create_anchors(edge)
+
+            edge.source_anchor = source_anchor
+            edge.target_anchor = target_anchor
+
+            source.anchors.append(source_anchor)
+            target.anchors.append(target_anchor)
+
+    def __create_anchors(self, edge):
+        target_anchor = Anchor()
+        source_anchor = Anchor()
+        source, target = self.__get_nodes(edge)
+
+        source_anchor.connector = target_anchor.connector = edge
+        source_anchor.slot = source
+        target_anchor.slot = target
+
+        return source_anchor, target_anchor
+
+    def __get_nodes(self, edge):
+        source = self.nodes[edge.uml_object['source-object']]
+        target = self.nodes[edge.uml_object['target-object']]
+        return source, target
+
 
     def clear(self):
         super(Diagram, self).clear()
--- omelette/fromage/test/test_diagram.py
+++ omelette/fromage/test/test_diagram.py
@@ -2,6 +2,7 @@ import unittest
 from mock import Mock
 from omelette.fromage.diagram import Diagram
 import omelette.fromage.test.qapp
+from omelette.fromage.common import DrawableNode, DrawableEdge
 
 class DiagramTest(unittest.TestCase):
 
@@ -36,6 +37,52 @@ class DiagramTest(unittest.TestCase):
 
         self.assertFalse("test" in self.diagram.edges)
         self.assertFalse("test" in self.diagram.nodes)
+
+    def test_anchor(self):
+        target = Mock()
+        target.type = "node"
+
+        source = Mock()
+        source.type = "node"
+
+        relation = Mock()
+        relation.type = "edge"
+
+        # given two node objects connected by an edge object
+        
+        target.name = "target"
+        source.name = "source"
+
+        properties = {"source-object": "source", "target-object": "target"}
+
+        def getitem(name):
+            return properties[name]
+
+        relation.__getitem__ = Mock(side_effect=getitem)
+        relation.name = "relation"
+
+        # added to diagram
+
+        self.diagram.add(relation)
+        self.diagram.add(target)
+        self.diagram.add(source)
+
+        # when anchors are set 
+
+        self.diagram.set_anchors()
+
+        # each DrawableNode object should be referenced by proper DrawableEdge object's anchors
+        drawable_relation = self.diagram.edges["relation"]
+        drawable_source = self.diagram.nodes["source"]
+        drawable_target = self.diagram.nodes["target"]
+
+        self.assertEquals(drawable_source, drawable_relation.source_anchor.slot)
+        self.assertEquals(drawable_target, drawable_relation.target_anchor.slot)
+
+        # and each DrawableNode object's anchor should reference the
+        # DrawableEdge object
+        self.assertEquals(drawable_relation, drawable_source.anchors.pop().connector)
+        self.assertEquals(drawable_relation, drawable_target.anchors.pop().connector)
 
 
 if __name__ == "__main__":
