b61622b053a39582400dd40491889415a1869f14
Barnaba Turek <barnabaturek@gmail.com>
2011-04-11 12:40:07 +0200

    framework for Logger class and test for #25

--- omelette/compiler/test/test_compiler.py
+++ omelette/compiler/test/test_compiler.py
@@ -1,6 +1,7 @@
 import unittest
 from omelette.compiler.code import Code
 from omelette.compiler.compiler import Compiler
+from omelette.compiler import logger
 
 class CompilerTest(unittest.TestCase):
 
@@ -48,6 +49,17 @@ class CompilerTest(unittest.TestCase):
         self.assertEquals(len(result), 1)
         self.assertEquals(result.keys().pop(), "Student")
 
+class CompilerIntegrationTest(unittest.TestCase):
+
+    def setUp(self):
+        logger.instance.clear()
+        self.instance = Compiler()
+
+    def test_circular_refference(self):
+        code = Code("objectname objectname")
+        self.instance.compile(code)
+        self.assertTrue(logger.instance.has_errors, 
+        "logger should contain errors")
 
 if __name__ == "__main__":
     unittest.main()
