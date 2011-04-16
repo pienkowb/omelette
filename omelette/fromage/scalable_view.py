00a643fdcb7bdc65eaac8488dbaf4d5b8c1fcbbe
Slawomir Blatkiewicz <slave123@vp.pl>
2011-04-16 16:39:18 +0200

    Now Ctrl+Mouse_Wheel works when we scale diagram

--- omelette/fromage/scalable_view.py
+++ omelette/fromage/scalable_view.py
@@ -7,6 +7,27 @@ class ScalableView(QtGui.QGraphicsView):
         self._current_center = QtCore.QPointF(0, 0)
         self.set_center(QtCore.QPointF(200, 200))
 
+        """#pan point
+        self.last_point = QtCore.QPoint(0, 0)
+        self.setCursor(QtGui.QCursor(17))"""
+
+        self._event_type = ""
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Control:
+            event_type = "pressed"
+            self.set_event_type(event_type)
+
+    def keyReleaseEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Control:
+            event_type = "release"
+            self.set_event_type(event_type)
+
+    def set_event_type(self, event_type):
+        self._event_type = event_type
+
+    def get_event_type(self):
+        return self._event_type
 
     def get_center(self):
         return self._current_center
@@ -42,22 +63,41 @@ class ScalableView(QtGui.QGraphicsView):
         self.centerOn(self._current_center)
 
     def wheelEvent(self, event):
-        point_before_scale = self.mapToScene(event.pos())
-        screen_center = self.get_center()
+        if self.get_event_type() == "pressed" and not self.get_event_type() == "release":
+            point_before_scale = self.mapToScene(event.pos())
+            screen_center = self.get_center()
 
-        scale_factor = 1.15
-        if event.delta() > 0:
-            self.scale(scale_factor, scale_factor)
+            scale_factor = 1.15
+            if event.delta() > 0:
+                self.scale(scale_factor, scale_factor)
+            else:
+                self.scale(1.0 / scale_factor, 1.0 / scale_factor)
+
+            point_after_scale = self.mapToScene(event.pos())
+            offset = point_before_scale - point_after_scale
+            new_center = screen_center + offset
+            self.set_center(point_after_scale)#new_center)
         else:
-            self.scale(1.0 / scale_factor, 1.0 / scale_factor)
-
-        point_after_scale = self.mapToScene(event.pos())
-        offset = point_before_scale - point_after_scale
-        new_center = screen_center + offset
-        self.set_center(point_after_scale)#new_center)
+            return
 
     def resizeEvent(self, event):
         visible_area = self.mapToScene(self.rect()).boundingRect()
         self.set_center(visible_area.center())
 
         super(ScalableView, self).resizeEvent(event)
+"""
+    def mousePressEvent(self, event):
+        self.last_point = event.pos()
+        self.setCursor(QtGui.QCursor(18))
+
+    def mouseReleaseEvent(self, event):
+        self.setCursor(QtGui.QCursor(17))
+        self.last_point = QtCore.QPoint(0, 0)
+
+    def mouseMoveEvent(self, event):
+        if not self.last_point.isNull():
+            delta = self.mapToScene(self.last_point) - self.mapToScene(event.pos())
+            self.last_point = event.pos()
+
+            self.set_center(self.get_center() + delta)
+"""
