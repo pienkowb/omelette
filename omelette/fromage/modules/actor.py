2367f0759b4bd3fd8dd180e0efae11bc1e44b2cb
Michal Zochniak <michal@zapu.net>
2011-04-27 17:43:38 +0200

    Actor refactoring. Actor drawing is now based on __actor_rectangle

--- omelette/fromage/modules/actor.py
+++ omelette/fromage/modules/actor.py
@@ -11,55 +11,49 @@ class DrawableActor(DrawableNode, QGraphicsItem):
     def __init__(self, uml_object):
         super(DrawableActor, self).__init__(uml_object)
         QGraphicsItem.__init__(self)
-        self.__boundingRect = QRectF(0, 0, 100, 100)
+        self.__bounding_rect = QRectF(0, 0, 100, 100)
         self.__font = QFont('Comic Sans MS', 10)
         self.__sectionMargin = 5
-        self.__textMargin = 3
+        self.__text_margin = 3
 
         self.setFlag(QGraphicsItem.ItemIsMovable, 1)
         self.setFlag(QGraphicsItem.ItemIsSelectable, 1)
         self.setFlag(QGraphicsItem.ItemSendsGeometryChanges, 1)
         
-        self.__actor_rectangle = QRectF(0, 0, 50, 100)
+        self.__actor_rectangle = QRectF(0, 0, 40, 80)
         
         self.__actor_name_text = DrawableText.create_drawable_text(self)
 
     def boundingRect(self):
-        return self.__boundingRect
+        return self.__bounding_rect
 
     def paint(self, painter, style, widget):
         metrics = QFontMetrics(self.__font)
         painter.setFont(self.__font)
         painter.setPen(QColor(0, 0, 0))
-        painter.fillRect(QRectF(self.__boundingRect), QBrush(QColor(255, 255, 255), Qt.SolidPattern))
+        painter.fillRect(QRectF(self.__bounding_rect), QBrush(QColor(255, 255, 255), Qt.SolidPattern))
 
-        # TODO: Make lines dependent on __actor_rectangle
         painter.setRenderHint(QPainter.Antialiasing, True)
         
-        painter.drawEllipse(10,0,30,30)
+        head_rad = math.ceil(self.__actor_rectangle.height() / 3)
+        center = self.__actor_rectangle.width() / 2
+        head_start = center - head_rad / 2
         
-        painter.drawLine(0, 40, 50, 40)
-        painter.drawLine(25, 30, 25, 70)
+        painter.drawEllipse(head_start,0,head_rad,head_rad)
         
-        painter.drawLine(25, 70, 0, 100)
-        painter.drawLine(25, 70, 50, 100)
+        painter.drawLine(0, head_rad * 1.3, self.__actor_rectangle.width(), head_rad * 1.3)
+        painter.drawLine(center, head_rad, center, head_rad*2)
+        
+        painter.drawLine(center, head_rad * 2, 0, self.__actor_rectangle.height())
+        painter.drawLine(center, head_rad * 2, self.__actor_rectangle.width(), self.__actor_rectangle.height())
         
         painter.setRenderHint(QPainter.Antialiasing, False)
         
 
-    def __sizeOfSection(self, metrics, list):
-        count = 0
-        maxWidth = 0
-        for text in list:
-            maxWidth = max(maxWidth, 2 * self.__textMargin + metrics.width(text))
-            count += 1
-
-        return (maxWidth, 2 * self.__sectionMargin + (count - 1) * self.__textMargin + count * metrics.height())
-
     def update(self):
         metrics = QFontMetrics(self.__font)
         
-        self.__boundingRect = QRectF(self.__actor_rectangle)
+        self.__bounding_rect = QRectF(self.__actor_rectangle)
         globalRect = self.globalBoundingRect()
         
         # TODO: This again
@@ -67,7 +61,8 @@ class DrawableActor(DrawableNode, QGraphicsItem):
             self.uml_object["name"] = self.uml_object.name
             
         self.__actor_name_text.text = self.uml_object["name"]
-        self.__actor_name_text.origin_pos = QPointF(globalRect.x() + globalRect.width() / 2, globalRect.y() + globalRect.height())
+        self.__actor_name_text.origin_pos = QPointF(globalRect.x() + globalRect.width() / 2, 
+                                                    globalRect.y() + globalRect.height())
         self.__actor_name_text.reset_vector = QPointF(- metrics.width(self.uml_object["name"]) / 2, 0)
         if(not self.__actor_name_text.isVisible()):
             self.__actor_name_text.reset_pos()
@@ -79,17 +74,17 @@ class DrawableActor(DrawableNode, QGraphicsItem):
         vertexes = [global_rect.topLeft(), global_rect.topRight(),
                   global_rect.bottomRight(), global_rect.bottomLeft()]
         
-        intersectionPoint = QPointF()
+        intersection_point = QPointF()
         
         # Iterate over pairs of vertexes that make rectangle edges
         for (a, b) in [(0, 1), (1, 2), (2, 3), (3, 0)]:
             bok = QLineF(vertexes[a], vertexes[b])
-            itype = line.intersect(bok, intersectionPoint)
+            itype = line.intersect(bok, intersection_point)
             if(itype == QLineF.BoundedIntersection):
                 if(line_point == 0):
-                    return QLineF(intersectionPoint, line.p2())
+                    return QLineF(intersection_point, line.p2())
                 else:
-                    return QLineF(line.p1(), intersectionPoint)
+                    return QLineF(line.p1(), intersection_point)
         
         return line
         
