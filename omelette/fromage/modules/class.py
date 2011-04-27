e8d436ff42c1a3cf17f2ab3a97384ef427609eb2
Michal Zochniak <michal@zapu.net>
2011-04-27 13:38:29 +0200

    DrawableClass refactoring (method/attribute/locals renaming)

--- omelette/fromage/modules/class.py
+++ omelette/fromage/modules/class.py
@@ -8,19 +8,20 @@ class DrawableClass(DrawableNode, QGraphicsItem):
     def __init__(self, uml_object):
         super(DrawableClass, self).__init__(uml_object)
         QGraphicsItem.__init__(self)
-        self.__boundingRect = QRectF(0, 0, 100, 100)
+        self.__bounding_rect = QRectF(0, 0, 100, 100)
         self.__font = QFont('Comic Sans MS', 10)
-        self.__sectionMargin = 5
-        self.__textMargin = 3
+        self.__section_margin = 5
+        self.__text_margin = 3
 
         self.setFlag(QGraphicsItem.ItemIsMovable, 1)
         self.setFlag(QGraphicsItem.ItemIsSelectable, 1)
         self.setFlag(QGraphicsItem.ItemSendsGeometryChanges, 1)
 
     def boundingRect(self):
-        return self.__boundingRect
+        return self.__bounding_rect
 
     def __draw_oa(self, painter, current_height, list):
+        """ Used to draw operation or attribute list """
         metrics = QFontMetrics(self.__font)
         for obj in list:       
             print str(obj) + " " + str(obj.is_static)    
@@ -29,7 +30,7 @@ class DrawableClass(DrawableNode, QGraphicsItem):
                 font.setUnderline(True)
                 painter.setFont(font)                
             
-            painter.drawText(QRect(self.__textMargin, current_height, metrics.width(str(obj)), metrics.height()), 0, str(obj))
+            painter.drawText(QRect(self.__text_margin, current_height, metrics.width(str(obj)), metrics.height()), 0, str(obj))
             
             # Set font back to non-underlined
             if(obj.is_static):
@@ -37,7 +38,7 @@ class DrawableClass(DrawableNode, QGraphicsItem):
                 font.setUnderline(False)
                 painter.setFont(font)
                 
-            current_height += self.__textMargin + metrics.height() 
+            current_height += self.__text_margin + metrics.height() 
             
         return current_height
 
@@ -45,71 +46,72 @@ class DrawableClass(DrawableNode, QGraphicsItem):
         metrics = QFontMetrics(self.__font)
         painter.setFont(self.__font)
         painter.setPen(QColor(0, 0, 0))
-        painter.fillRect(QRectF(self.__boundingRect), QBrush(QColor(255, 255, 255), Qt.SolidPattern))
+        painter.fillRect(QRectF(self.__bounding_rect), QBrush(QColor(255, 255, 255), Qt.SolidPattern))
 
         # Name of the class        
-        currentHeight = self.__sectionMargin
-        painter.drawText(QRect(self.__textMargin, currentHeight, metrics.width(self.uml_object['name']), metrics.height()), 0, self.uml_object['name'])
-        currentHeight += metrics.height()
+        current_height = self.__section_margin
+        painter.drawText(QRect(self.__text_margin, current_height, metrics.width(self.uml_object['name']), metrics.height()), 0, self.uml_object['name'])
+        current_height += metrics.height()
         
         # Stereotype
         if("stereotype" in self.uml_object.properties):
-            currentHeight += self.__textMargin
+            current_height += self.__text_margin
             stereotype = "<< " + self.uml_object["stereotype"] + " >>"
-            painter.drawText(QRect(self.__textMargin, currentHeight, metrics.width(stereotype), metrics.height()), 0, stereotype)
-            currentHeight += metrics.height()
+            painter.drawText(QRect(self.__text_margin, current_height, metrics.width(stereotype), metrics.height()), 0, stereotype)
+            current_height += metrics.height()
             
-        currentHeight += self.__sectionMargin
+        current_height += self.__section_margin
         
         # Attributes section
-        painter.drawLine(0, currentHeight, self.__boundingRect.width(), currentHeight)
-        currentHeight += self.__sectionMargin
+        painter.drawLine(0, current_height, self.__bounding_rect.width(), current_height)
+        current_height += self.__section_margin
         
-        currentHeight = self.__draw_oa(painter, currentHeight, self.uml_object.attributes()) 
+        current_height = self.__draw_oa(painter, current_height, self.uml_object.attributes()) 
         
         # Operations section
-        painter.drawLine(0, currentHeight, self.__boundingRect.width(), currentHeight)
+        painter.drawLine(0, current_height, self.__bounding_rect.width(), current_height)
         
-        currentHeight += self.__sectionMargin
-        currentHeight = self.__draw_oa(painter, currentHeight, self.uml_object.operations())
+        current_height += self.__section_margin
+        current_height = self.__draw_oa(painter, current_height, self.uml_object.operations())
 
-        painter.drawRect(self.__boundingRect)
+        painter.drawRect(self.__bounding_rect)
 
-    def __sizeOfSection(self, metrics, list):
+    def __size_of_section(self, metrics, list):
+        """ Used to determine size of section - attribute or operation list. """
         count = 0
         maxWidth = 0
         for text in list:
-            maxWidth = max(maxWidth, 2 * self.__textMargin + metrics.width(text))
+            maxWidth = max(maxWidth, 2 * self.__text_margin + metrics.width(text))
             count += 1
 
-        return (maxWidth, 2 * self.__sectionMargin + (count - 1) * self.__textMargin + count * metrics.height())
+        return (maxWidth, 2 * self.__section_margin + (count - 1) * self.__text_margin + count * metrics.height())
 
     def update(self):
         metrics = QFontMetrics(self.__font)
         # Start by finding size of class name block
-        drawableHeight = 1 * self.__sectionMargin + metrics.height()
+        drawable_height = 1 * self.__section_margin + metrics.height()
 
         # TODO: This doesn't belong here. Or does it?
         if "name" not in self.uml_object.properties:
             self.uml_object["name"] = self.uml_object.name
 
-        drawableWidth = 2 * self.__textMargin + metrics.width(self.uml_object['name'])
+        drawable_width = 2 * self.__text_margin + metrics.width(self.uml_object['name'])
         
         # Add stereotype height
         if("stereotype" in self.uml_object.properties):
-            drawableHeight += self.__textMargin + metrics.height()
+            drawable_height += self.__text_margin + metrics.height()
             
-            stereotypeWidth = 2 * self.__textMargin + metrics.width("<< " + self.uml_object['stereotype'] + " >>")
-            drawableWidth = max(drawableWidth, stereotypeWidth)
+            stereotypeWidth = 2 * self.__text_margin + metrics.width("<< " + self.uml_object['stereotype'] + " >>")
+            drawable_width = max(drawable_width, stereotypeWidth)
 
         # Find sizes of each section and update width/height
         for section in [map(str, self.uml_object.operations()), map(str,
                                                                     self.uml_object.attributes())]:
-            size = self.__sizeOfSection(metrics, section)
-            drawableWidth = max(drawableWidth, size[0])
-            drawableHeight += size[1]
+            size = self.__size_of_section(metrics, section)
+            drawable_width = max(drawable_width, size[0])
+            drawable_height += size[1]
 
-        self.__boundingRect = QRectF(0, 0, 2 * self.__textMargin + drawableWidth, drawableHeight)
+        self.__bounding_rect = QRectF(0, 0, 2 * self.__text_margin + drawable_width, drawable_height)
 
     def crop_line(self, line, line_point):
         global_rect = self.globalBoundingRect()
@@ -117,17 +119,17 @@ class DrawableClass(DrawableNode, QGraphicsItem):
         vertexes = [global_rect.topLeft(), global_rect.topRight(),
                   global_rect.bottomRight(), global_rect.bottomLeft()]
         
-        intersectionPoint = QPointF()
+        intersection_point = QPointF()
         
         # Iterate over pairs of vertexes that make rectangle edges
         for (a, b) in [(0, 1), (1, 2), (2, 3), (3, 0)]:
             bok = QLineF(vertexes[a], vertexes[b])
-            itype = line.intersect(bok, intersectionPoint)
+            itype = line.intersect(bok, intersection_point)
             if(itype == QLineF.BoundedIntersection):
                 if(line_point == 0):
-                    return QLineF(intersectionPoint, line.p2())
+                    return QLineF(intersection_point, line.p2())
                 else:
-                    return QLineF(line.p1(), intersectionPoint)
+                    return QLineF(line.p1(), intersection_point)
         
         return line
         
